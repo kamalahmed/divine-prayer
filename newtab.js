6599c5eee6c2945e19daa869def97201c2a08179
Kamal <kamalacca@gmail.com>
2024-07-09 00:30:55 +0400

    update style of the verse and add arabic

--- newtab.js
+++ newtab.js
@@ -1,55 +1,6 @@
-document.addEventListener("xlosdingDOMContentLoaded", () => {
-  const verseElement = document.getElementById("verse");
-  const changeWallpaperButton = document.getElementById("change-wallpaper");
-
-  const count = 19;
-  const wallpapers = [];
-  for (let i = 0; i < count; i++) {
-    const wallpaperURL = `wallpapers/wallpaper${i + 1}.jpg`;
-    wallpapers.push(wallpaperURL);
-  }
-
-  const verses = [
-    "Indeed, this Qur'an guides to that which is most suitable...",
-    "And We have certainly made the Qur'an easy for remembrance...",
-    // Add more verses here
-  ];
-
-  function getRandomVerse() {
-    const randomIndex = Math.floor(Math.random() * verses.length);
-    return verses[randomIndex];
-  }
-
-  function getRandomWallpaper() {
-    const randomIndex = Math.floor(Math.random() * wallpapers.length);
-    return wallpapers[randomIndex];
-  }
-
-  function displayVerse() {
-    verseElement.textContent = getRandomVerse();
-  }
-
-  function changeWallpaper() {
-    const randomWallpaper = getRandomWallpaper();
-    document.body.style.backgroundImage = `url(${randomWallpaper})`;
-    localStorage.setItem("wallpaper", randomWallpaper);
-  }
-
-  changeWallpaperButton.addEventListener("click", changeWallpaper);
-
-  const savedWallpaper = localStorage.getItem("wallpaper");
-  if (savedWallpaper) {
-    document.body.style.backgroundImage = `url(${savedWallpaper})`;
-  } else {
-    changeWallpaper(); // Set a random wallpaper on the first load
-  }
-
-  // Display a verse when the new tab is opened
-  displayVerse();
-});
-
 document.addEventListener("DOMContentLoaded", () => {
   const verseElement = document.getElementById("verse");
+  const verseEnElement = document.getElementById("verse-en");
   const changeWallpaperButton = document.getElementById("change-wallpaper");
   const wallpaperSelector = document.getElementById("wallpaper-selector");
   const count = 19;
@@ -60,9 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const verses = [
-    "Indeed, this Qur'an guides to that which is most suitable...",
-    "And We have certainly made the Qur'an easy for remembrance...",
-    // Add more verses here
+    {
+      ar: "الحَمْـدُ لِلّهِ الّذي أَحْـيانا بَعْـدَ ما أَماتَـنا وَإليه النُّـشور",
+      en: "All praise is for Allah who gave us life after having taken it from us and unto Him is the resurrection.",
+    },
+    {
+      ar: "(بِسْمِ الله ) اللّهُـمَّ إِنِّـي أَعـوذُ بِـكَ مِـنَ الْخُـبْثِ وَالْخَبائِث",
+      en: "(In the name of Allah). O Allah, I take refuge with you from all evil and evil-doers.",
+    },
+    {
+      ar: "لا إلهَ إلاّ اللّه وحدَهُ لا شريكَ لهُ، لهُ المُلْـكُ ولهُ الحَمْـد، يُحْيـي وَيُميـتُ وَهُوَ حَيٌّ لا يَمـوت، بِيَـدِهِ الْخَـيْرُ وَهوَ على كلّ شيءٍ قدير",
+      en: "None has the right to be worshipped except Allah, alone, without partner, to Him belongs all sovereignty and praise. He gives life and causes death, and He is living and does not die. In His hand is all good and He is over all things, omnipotent.",
+    },
   ];
 
   function getRandomVerse() {
@@ -76,7 +36,9 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function displayVerse() {
-    verseElement.textContent = getRandomVerse();
+    let verse = getRandomVerse();
+    verseElement.textContent = verse.ar;
+    verseEnElement.textContent = verse.en;
   }
 
   function changeWallpaper() {
